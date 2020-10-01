56e91513408a420c52e792551a28ff2509b6f54f
Taylor Fritz <tayfritz01@gmail.com>
2020-10-01 19:26:14 -0400

    books.py created and subclass created on a dictionary

---
+++ javascriptobject.py
@@ -0,0 +1,6 @@
+class JavascriptObject(dict):
+    def __getattribute__(self, item):
+        try:
+            return self[item]
+        except KeyError:
+            return super().__getattribute__(item)
