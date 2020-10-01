ec06e2d12a85468c52829ae202cd8ad1d1739260
Taylor Fritz <tayfritz01@gmail.com>
2020-10-01 17:02:42 -0400

    Character subclass exercises to learn about built ins

--- rpg/inventory.py
+++ rpg/inventory.py
@@ -1,2 +1,15 @@
 class Inventory:
-    pass
+    def __init__(self):
+        self.slots = []
+    
+    def add(self, item):
+        self.slots.append(item)
+    
+    def __len__(self):
+        return len(self.slots)
+
+    def __contains__(self, item):
+        return item in self.slots
+    
+    def __iter__(self):
+        yield from self.slots
