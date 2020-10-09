0cf2593340f11475943a5f67013f26a96d7921b6
Taylor Fritz <tayfritz01@gmail.com>
2020-10-08 20:47:02 -0400

    Classes and methods added to hands.py to create YatzyHand()

--- yatzy/diceroller.py
+++ yatzy/diceroller.py
@@ -36,6 +36,9 @@ class Die:
     def __radd__(self, other):
         return int(self) + other
 
+    def __repr__(self):
+        return str(self.value)
+
 class D6(Die):
     def __init__(self, value=0):
         super().__init__(sides=6, value=value)
--- yatzy/hands.py
+++ yatzy/hands.py
@@ -1,3 +1,16 @@
+from diceroller import D6
+
 class Hand(list):
-    def __init__(self, size=0, *args, **kwargs):
+    def __init__(self, size=0, die_class=None, *args, **kwargs):
+        if not die_class:
+            raise ValueError("You must provide a die class.")
         super().__init__()
+
+        for x in range(size):
+            self.append(die_class())
+        self.sort()
+
+class YatzyHand(Hand):
+    def __init__(self, *args, **kwargs):
+        super().__init__(size=5, die_class=D6, *args, **kwargs)
+    
